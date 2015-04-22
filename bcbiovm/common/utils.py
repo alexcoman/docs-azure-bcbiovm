0817cc404f9dc92be9cbe4f39ea5d76caf56709b
Alexandru Coman <acoman@cloudbasesolutions.com>
2015-04-22 15:07:15 +0300

    Add decorator for exposing custom methods

--- bcbiovm/common/utils.py
+++ bcbiovm/common/utils.py
@@ -1,5 +1,63 @@
 """Utilities and helper functions."""
 
+import types
+
+import six
+
+
+def expose(function=None, alias=None):
+    """
+    Expose the function, optionally providing an alias or set of aliases.
+
+    This decorator will be use to expose to the API the custom methods.
+    Examples:
+    ::
+        @expose
+        def compute_something(self):
+            # ...
+            pass
+
+        @expose(alias="something")
+        def compute_something(self):
+            # ...
+            pass
+
+        @expose(alias=["something", "do_something"])
+        def compute_something(self):
+            # ...
+            pass
+
+        @expose(["something", "do_something"]):
+        def compute_something(self):
+            # ...
+            pass
+    """
+
+    def get_alias():
+        if alias is None:
+            return ()
+        elif isinstance(alias, six.string_types):
+            return (alias)
+        else:
+            return tuple(alias)
+
+    def wrapper(func):
+        func.exposed = True
+        func.alias = get_alias()
+        return func
+
+    if isinstance(function, (types.FunctionType, types.MethodType)):
+        function.exposed = True
+        function.alias = get_alias()
+        return function
+
+    elif function is None:
+        return wrapper
+
+    else:
+        alias = function
+        return wrapper
+
 
 def load_class(class_path):
     try:
